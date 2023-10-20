ac2e691dd64e622bfdd1b5d9da1324ba8267b270
Nick Ward <nickward13@hotmail.com>
2023-10-20 04:48:11 +0000

    added dotnet health scenario

---
+++ notebooks/dotnet/config/StringUtils.cs
@@ -0,0 +1,30 @@
+using System.IO;
+using System.Text.Json;
+
+public static class StringUtils
+{
+    // Function used to wrap long lines of text
+    public static string WordWrap(string text, int maxLineLength)
+    {
+        var result = new StringBuilder();
+        int i;
+        var last = 0;
+        var space = new[] { ' ', '\r', '\n', '\t' };
+        do
+        {
+            i = last + maxLineLength > text.Length
+                ? text.Length
+                : (text.LastIndexOfAny(new[] { ' ', ',', '.', '?', '!', ':', ';', '-', '\n', '\r', '\t' }, Math.Min(text.Length - 1, last + maxLineLength)) + 1);
+            if (i <= last) i = Math.Min(last + maxLineLength, text.Length);
+            result.AppendLine(text.Substring(last, i - last).Trim(space));
+            last = i;
+        } while (i < text.Length);
+
+        return result.ToString();
+    }
+
+    public static void Print(string text)
+    {
+        Console.WriteLine(WordWrap(text, 80));
+    }
+}
