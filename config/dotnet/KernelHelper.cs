fd51bf6b675d6da992ec3c18be5f48e95e49c173
Rohit Lakhanpal <rohit-lakhanpal@users.noreply.github.com>
2023-11-05 20:36:22 +1100

    Added all Core Plugins and Bing Search Plugin

--- config/dotnet/KernelHelper.cs
+++ config/dotnet/KernelHelper.cs
@@ -1,5 +1,8 @@
 using Microsoft.SemanticKernel;
 using Microsoft.SemanticKernel.Reliability.Basic;
+using Microsoft.SemanticKernel.Plugins.Core;
+using Microsoft.SemanticKernel.Plugins.Web;
+using Microsoft.SemanticKernel.Plugins.Web.Bing;
 
 public static class KernelHelper
 {
@@ -72,6 +75,75 @@ public static class KernelHelper
             UseExponentialBackoff = true,
         });
 
-        return builder.Build();
+        var kernel = builder.Build();
+
+        // Add the Bing search engine if the API key is provided.
+        if(configuration.BingSearchConfig != null && !string.IsNullOrEmpty(configuration.BingSearchConfig.ApiKey))
+        {
+            kernel.ImportFunctions(new WebSearchEnginePlugin(new BingConnector(configuration.BingSearchConfig.ApiKey)), "bing");
+        }
+
+        // Adding Text Plugin. See: https://github.com/microsoft/semantic-kernel/blob/main/dotnet/src/Plugins/Plugins.Core/TextPlugin.cs
+        // SKContext.Variables["input"] = "  hello world  "
+        // {{text.trim $input}} => "hello world"
+        // {{text.trimStart $input} => "hello world  "
+        // {{text.trimEnd $input} => "  hello world"
+        // SKContext.Variables["input"] = "hello world"
+        // {{text.uppercase $input}} => "HELLO WORLD"
+        // SKContext.Variables["input"] = "HELLO WORLD"
+        // {{text.lowercase $input}} => "hello world"
+        kernel.ImportFunctions(new TextPlugin(), "text");
+
+        // Adding Time Plugin. See: https://github.com/microsoft/semantic-kernel/blob/main/dotnet/src/Plugins/Plugins.Core/TimePlugin.cs
+        // Examples:
+        // {{time.date}}            => Sunday, 12 January, 2031
+        // {{time.today}}           => Sunday, 12 January, 2031
+        // {{time.now}}             => Sunday, January 12, 2031 9:15 PM
+        // {{time.utcNow}}          => Sunday, January 13, 2031 5:15 AM
+        // {{time.time}}            => 09:15:07 PM
+        // {{time.year}}            => 2031
+        // {{time.month}}           => January
+        // {{time.monthNumber}}     => 01
+        // {{time.day}}             => 12
+        // {{time.dayOfMonth}}      => 12
+        // {{time.dayOfWeek}}       => Sunday
+        // {{time.hour}}            => 9 PM
+        // {{time.hourNumber}}      => 21
+        // {{time.daysAgo $days}}   => Sunday, January 12, 2025 9:15 PM
+        // {{time.lastMatchingDay $dayName}} => Sunday, 7 May, 2023
+        // {{time.minute}}          => 15
+        // {{time.minutes}}         => 15
+        // {{time.second}}          => 7
+        // {{time.seconds}}         => 7
+        // {{time.timeZoneOffset}}  => -08:00
+        // {{time.timeZoneName}}    => PST
+        kernel.ImportFunctions(new TimePlugin(), "time"); 
+
+        // Adding Wait Plugin. See: https://github.com/microsoft/semantic-kernel/blob/main/dotnet/src/Plugins/Plugins.Core/WaitPlugin.cs
+        // Examples:
+        // {{wait.seconds 10}} => Wait 10 seconds
+        kernel.ImportFunctions(new WaitPlugin(), "wait");
+
+        // Adding Math Plugin. See: https://github.com/microsoft/semantic-kernel/blob/main/dotnet/src/Plugins/Plugins.Core/MathPlugin.cs
+        // Examples:
+        // {{math.Add}} => FirstNumber + SecondNumber  (provided in the SKContext)
+        // {{math.Subtract}} => FirstNumber - SecondNumber (provided in the SKContext)
+        kernel.ImportFunctions(new MathPlugin(), "math");
+
+        // Adding Http Plugin. See: https://github.com/microsoft/semantic-kernel/blob/main/dotnet/src/Plugins/Plugins.Core/HttpPlugin.cs
+        // Examples:
+        // {{http.getAsync $url}}
+        // {{http.postAsync $url}}
+        // {{http.putAsync $url}}
+        // {{http.deleteAsync $url}}
+        kernel.ImportFunctions(new HttpPlugin(), "http");
+
+        // Adding FileIO Plugin. See: https://github.com/microsoft/semantic-kernel/blob/main/dotnet/src/Plugins/Plugins.Core/FileIOPlugin.cs
+        // Examples:
+        // {{file.readAsync $path }} => "hello world"
+        // {{file.writeAsync}}        
+        kernel.ImportFunctions(new FileIOPlugin(), "file");
+
+        return kernel;
     }
 }
